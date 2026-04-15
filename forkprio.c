90d150fae7a60dcad8e5c4b7155d382468713786
michiaguilarr <michiaguilarr@gmail.com>
2026-04-15 15:11:35 -0300

    separacion hijos/padres

--- forkprio.c
+++ forkprio.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/times.h>      // times()
+#include <time.h>
+#include <unistd.h>
 
-int busywork(void)
+int busywork(int copias)
 {
     struct tms buf;
-    for (;;) {
+    clock_t inicio = clock();
+    for (int i=0;i<copias;i++) 
+    {
+        pid_t hijo=fork();
+        if(hijo==0){
+
+            printf("soy el hijo %d \n",getpid());
+        
+        }
+        else{
+            sleep(20);
+            printf("soy el padre y cree el hijo %d \n", hijo);
+            
+        }
+        
         times(&buf);
     }
 }
 
 int main(int argc, char *argv[])
 {
+    busywork(argc);
+
     exit(EXIT_SUCCESS);
 }
