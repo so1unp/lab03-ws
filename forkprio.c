5c0ac9a5da9658d1bea7a12eebc236531542cf81
michiaguilarr <michiaguilarr@gmail.com>
2026-04-20 18:56:37 -0300

    sigaction implementacion (todavia no funciona bien)

--- forkprio.c
+++ forkprio.c
@@ -1,34 +1,63 @@
+
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/times.h>      // times()
+#include <sys/resource.h>
+#include <sys/times.h> // times()
 #include <time.h>
 #include <unistd.h>
 
-int busywork(int copias)
-{
-    struct tms buf;
-    clock_t inicio = clock();
-    for (int i=0;i<copias;i++) 
-    {
-        pid_t hijo=fork();
-        if(hijo==0){
+volatile sig_atomic_t unprocessedSig = 0;
 
-            printf("soy el hijo %d \n",getpid());
-        
-        }
-        else{
-            sleep(20);
-            printf("soy el padre y cree el hijo %d \n", hijo);
-            
-        }
-        
-        times(&buf);
+int busywork(void) {
+
+  struct sigaction act;
+  struct tms buf;
+  for (;;) {
+
+    if (unprocessedSig) {
+      unprocessedSig = 0;
+      struct rusage usage;
+      getrusage(RUSAGE_SELF, &usage);
+      printf("soy el hijo %d  tiempo activo %ld \n", getpid(),
+             usage.ru_stime.tv_sec);
+
+      exit(EXIT_SUCCESS);
     }
+  }
+}
+void handler(int signo) {
+    unprocessedSig = 1;
 }
 
-int main(int argc, char *argv[])
-{
-    busywork(argc);
+int main(int argc, char *argv[]) {
 
-    exit(EXIT_SUCCESS);
+    signal(SIGTERM,handler);
+
+  int copias = atoi(argv[1]);
+  struct tms buf;
+  pid_t hijo;
+  clock_t inicio = clock();
+
+  int tiempoMuerto = atoi(argv[2]);
+
+  for (int i = 0; i < copias; i++) {
+    hijo = fork();
+
+    if (hijo == 0) {
+      busywork();
+    }
+  }
+
+  sleep(tiempoMuerto);
+  kill(hijo, SIGTERM);
+
+
+  // aca asesino a todos los hijos, despues del tiempo
+  // prioridad: basicamente hacer que los procesos tengan
+  // prioridad en el tiempo de cpu
+
+  // poner un handle en p2, cuando llega la senal print del estatus
+
+  exit(EXIT_SUCCESS);
 }
